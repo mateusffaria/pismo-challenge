c7aaba62997a2fe48da191cb200783a0cb212075
Mateus Faria <mateusjose555@gmail.com>
2024-09-23 18:30:31 -0300

    test: created unit tests for request

--- internal/accounts/handlers/request/request.go
+++ internal/accounts/handlers/request/request.go
@@ -1,5 +1,27 @@
 package request
 
+import (
+	"fmt"
+
+	validate "github.com/go-playground/validator/v10"
+	"github.com/mateusffaria/pismo-challenge/internal/accounts/handlers/request/errors"
+)
+
 type UserAccountRequest struct {
-	DocumentNumber string `json:"document_number,omitempty"`
+	DocumentNumber string `json:"document_number,omitempty" validate:"required"`
+}
+
+func (uar *UserAccountRequest) Validate() error {
+	validator := validate.New(validate.WithRequiredStructEnabled())
+	err := validator.Struct(uar)
+	if err != nil {
+		var fields = make([]string, 0)
+		for _, err := range err.(validate.ValidationErrors) {
+			fields = append(fields, fmt.Sprintf("%s is %s", err.StructField(), err.ActualTag()))
+		}
+
+		return errors.NewBodyError(fields)
+	}
+
+	return nil
 }
