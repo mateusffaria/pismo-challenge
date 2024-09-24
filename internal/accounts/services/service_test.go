49f14422faff27ca4684713d5404fb55bf4df9d0
Mateus Faria <mateusjose555@gmail.com>
2024-09-24 11:58:42 -0300

    test: increase test coverage for service layer

--- internal/accounts/services/service_test.go
+++ internal/accounts/services/service_test.go
@@ -1,14 +1,17 @@
 package services
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/mateusffaria/pismo-challenge/internal/accounts/domains"
 	"github.com/mateusffaria/pismo-challenge/internal/accounts/handlers/request"
+	svcErrors "github.com/mateusffaria/pismo-challenge/internal/accounts/services/errors"
 	repositories_test "github.com/mateusffaria/pismo-challenge/test/accounts/repositories"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"gorm.io/gorm"
 )
 
 func TestShouldCreateUserAccountSuccessfully(t *testing.T) {
@@ -29,6 +32,32 @@ func TestShouldCreateUserAccountSuccessfully(t *testing.T) {
 	assert.Equal(t, res, accModel)
 }
 
+func TestShouldHandleAccountDuplicateErrors(t *testing.T) {
+	repoMock := repositories_test.NewAccountRepositoryMock()
+	repoMock.On("CreateUserAccount", mock.AnythingOfType("domains.Account")).Return(domains.Account{}, gorm.ErrDuplicatedKey)
+
+	svc := NewAccountService(repoMock)
+
+	_, err := svc.CreateUserAccount(request.UserAccountRequest{
+		DocumentNumber: "1234567890",
+	})
+
+	assert.ErrorIs(t, err, svcErrors.ErrAccountDuplicated)
+}
+
+func TestShouldHandleAccountCreationErrors(t *testing.T) {
+	repoMock := repositories_test.NewAccountRepositoryMock()
+	repoMock.On("CreateUserAccount", mock.AnythingOfType("domains.Account")).Return(domains.Account{}, fmt.Errorf("some error"))
+
+	svc := NewAccountService(repoMock)
+
+	_, err := svc.CreateUserAccount(request.UserAccountRequest{
+		DocumentNumber: "1234567890",
+	})
+
+	assert.ErrorIs(t, err, svcErrors.ErrInternalDatabaseError)
+}
+
 func TestShouldGetUserAccountSuccessfully(t *testing.T) {
 	repoMock := repositories_test.NewAccountRepositoryMock()
 	accModel := domains.Account{
@@ -43,4 +72,26 @@ func TestShouldGetUserAccountSuccessfully(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, res, accModel)
+}
+
+func TestShouldHandleAccountNotFound(t *testing.T) {
+	repoMock := repositories_test.NewAccountRepositoryMock()
+	repoMock.On("GetUserAccount", "1234567890").Return(domains.Account{}, gorm.ErrRecordNotFound)
+
+	svc := NewAccountService(repoMock)
+
+	_, err := svc.GetUserAccount("1234567890")
+
+	assert.ErrorIs(t, err, svcErrors.ErrAccountNotFound)
+}
+
+func TestShouldHandleGetAccountErrors(t *testing.T) {
+	repoMock := repositories_test.NewAccountRepositoryMock()
+	repoMock.On("GetUserAccount", "1234567890").Return(domains.Account{}, fmt.Errorf("some error"))
+
+	svc := NewAccountService(repoMock)
+
+	_, err := svc.GetUserAccount("1234567890")
+
+	assert.ErrorIs(t, err, svcErrors.ErrInternalDatabaseError)
 }
